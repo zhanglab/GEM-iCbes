f8d342302ee7515c3096340edf4567ca49273fa2
Weishu Zhao <weishuzhao@bluewaves.uri.edu>
2018-10-30 11:12:14 -0400

    run blastp again with the output of qlen slen

--- Weishu-genome_Cbes_Tsac/Cbes_genome_NCBI/blast/run_blast.sh
+++ Weishu-genome_Cbes_Tsac/Cbes_genome_NCBI/blast/run_blast.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-blastp -query ../31899.10/31899.10.PATRIC.faa -db Cbes_ncbi -out Cbes_blastp_out.tsv -evalue 0.0000001 -outfmt 6 
+blastp -query ../31899.10/31899.10.PATRIC.faa -db Cbes_ncbi -out Cbes_blastp_out_len.tsv -evalue 0.0000001 -outfmt "6 std qlen slen"
